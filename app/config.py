c8f00b885b6cb2681d2c9a2dee4d3a7a5ef52386
patrickNgare <patrickngare@gmail.com>
2018-01-28 21:37:09 +0300

    chnaged url

---
+++ app/config.py
@@ -0,0 +1,33 @@
+class Config:
+    
+    
+    
+
+
+    
+
+
+    BASE_NEWS_API_URL = 'https://newsapi.org/v2/sources?language=en&country={}&category={}&apiKey={}'
+    SOURCE_NEWS_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+
+    
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True    
