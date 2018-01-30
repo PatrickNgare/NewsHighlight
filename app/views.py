a3c23ec83b78d0d05ff2737095de45e57a494b80
patrickNgare <patrickngare@gmail.com>
2018-01-30 14:17:32 +0300

    indenation and creating new class

--- app/views.py
+++ app/views.py
@@ -1,40 +1,30 @@
 from flask import render_template
 from app import app
 from .request import get_sources, get_articles
-# TODO: May have to import the Articles class here
-#  Views
+
 
 
 @app.route('/')
 def index():
+    
+
+    spo = get_sources('us', 'sports')
+    biz = get_sources('us', 'business')
+    heal = get_sources('us', 'health')
+    sci = get_sources('us', 'science')
+    ent = get_sources('us', 'entertainment')
+    gen = get_sources('us', 'general')
+    biz = get_sources('us', 'business')
+    tech = get_sources('us', 'technology')
    
-    general_list = get_sources('us', 'general')
-    business_list = get_sources('us', 'business')
-    technology_list = get_sources('us', 'technology')
-    sports_list = get_sources('us', 'sports')
-    health_list = get_sources('us', 'health')
-    science_list = get_sources('us', 'science')
-    entertainment_list = get_sources('us', 'entertainment')
-    test_args = 'Working!'
-    return render_template('index.html',
-                           test_param=test_args,
-                           general=general_list,
-                           business=business_list,
-                           technology=technology_list,
-                           sports=sports_list,
-                           health=health_list,
-                           science=science_list,
-                           entertainment=entertainment_list)
+  
+    return render_template('index.html', general=gen,business=biz,technology=tech,sports=spo,health=heal,science=sci,
+            entertainment=ent)
 
 
 @app.route('/news/<id>')
 def news(id):
-    """
-    View articles page that returns the news article from a highlight
-    """
+    
     news_args = get_articles(id)
-    highlight_args = 'Route Working!!'
-    # name = f'{results_list}'
-    return render_template('new.html',
-                           highlight_param=highlight_args,
-                           news=news_args)
+
+    return render_template('new.html',news=news_args)
