508eada662c69dc33bf66d375bab5cfedc3316e2
patrickNgare <patrickngare@gmail.com>
2018-01-31 20:59:16 +0300

    changed the structure

---
+++ app/main/views.py
@@ -0,0 +1,30 @@
+from flask import render_template
+from  . import main
+from ..request import get_sources, get_articles
+
+
+
+@main.route('/')
+def index():
+    
+
+    spo = get_sources('us', 'sports')
+    biz = get_sources('us', 'business')
+    heal = get_sources('us', 'health')
+    sci = get_sources('us', 'science')
+    ent = get_sources('us', 'entertainment')
+    gen = get_sources('us', 'general')
+    biz = get_sources('us', 'business')
+    tech = get_sources('us', 'technology')
+   
+  
+    return render_template('index.html', general=gen,business=biz,technology=tech,sports=spo,health=heal,science=sci,
+            entertainment=ent)
+
+
+@main.route('/news/<id>')
+def news(id):
+    
+    news_args = get_articles(id)
+
+    return render_template('new.html',news=news_args)
