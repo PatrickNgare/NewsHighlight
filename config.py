b60f5ea95a39eff303d2273d54dc514578332d51
patrickNgare <patrickngare@gmail.com>
2018-01-31 21:00:31 +0300

    changed the structure

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+class Config:
+
+    BASE_NEWS_API_URL = 'https://newsapi.org/v2/sources?language=en&country={}&category={}&apiKey={}'
+    SOURCE_NEWS_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
+  
+    
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True    
+config_options={
+ 'development':DevConfig,
+ 'production' :ProdConfig
+
+}
